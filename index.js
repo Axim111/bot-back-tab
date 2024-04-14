5adba12c38580970a6ff2cf5699e8ca51140fa72
Axim <makseropolov@gmail.com>
2024-04-14 23:28:42 +0500

    with webHook

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import TelegramBot from "node-telegram-bot-api"
+// import TelegramBot from "node-telegram-bot-api"
+import { Telegraf } from "telegraf"
 
 import express from 'express'
 
@@ -8,10 +9,14 @@ const webUrl = "https://gorgeous-dusk-a3880d.netlify.app/"
 //t.me/testo_ax_my_bot
 
 
-const bot = new TelegramBot(token);
-let urlHook = "https://api.render.com/deploy/srv-codtor0l6cac73bqc05g?key=8ExSdyDod9E"
-bot.setWebHook(urlHook + "bot" + `${token}`)
-bot.startWebHook(urlHook + "bot" + `${token}`, null, 4000)
+const bot = new Telegraf(token);
+
+
+
+
+
+
+bot.start((ctx) => ctx.reply('Welcome'))
 
 const commands = [
   {
@@ -41,15 +46,15 @@ const commands = [
 
 ]
 
-bot.setMyCommands(commands);
+bot.telegram.setMyCommands(commands);
 bot.on('message', async (msg) => {
-  const text = msg.text
+  const text = msg.message.text
   const chatId = msg.chat.id
-  console.log(msg)
+  // console.log(msg)
 
   if (msg.text == '/menu') {
 
-    await bot.sendMessage(msg.chat.id, `Меню бота`, {
+    await bot.telegram.sendMessage(msg.chat.id, `Меню бота`, {
       reply_markup: {
         inline_keyboard: [
           [{ text: 'Кнопка 1', web_app: { url: webUrl } }],
@@ -63,6 +68,12 @@ bot.on('message', async (msg) => {
 
   }
   else if (msg.text == "1") {
-    bot.sendMessage(msg.chat.id, `бан`)
+    bot.telegram.sendMessage(msg.chat.id, `бан`)
   }
 });
+
+
+
+let urlHook = "https://5d70-37-140-17-7.ngrok-free.app"
+bot.telegram.setWebhook(urlHook + "/bot" + `${token}`)
+bot.startWebhook(urlHook + "/bot" + `${token}`, null, 8800)
